a1ec0e9931e91788dcafd6a1e371ca1aaf5137c1
tsu__ko <natsu.nhm04@gmail.com>
2025-07-16 23:54:22 +0900

    Use test DB in CI & add coverage report with pytest-cov

--- budgetbook/app.py
+++ budgetbook/app.py
@@ -5,7 +5,11 @@ import os
 
 app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "db", "budgetbook.db")
+
+if os.getenv("TESTING") == "1":
+    DB_PATH = os.path.join(BASE_DIR, "db", "budgetbook_test.db")
+else:
+    DB_PATH = os.path.join(BASE_DIR, "db", "budgetbook.db")
 
 
 # --- 共通ヘルパー ---
