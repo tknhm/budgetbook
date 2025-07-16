b13cb6cfdb519435b1d782fdeb2b5fb3e88ecd65
tsu__ko <natsu.nhm04@gmail.com>
2025-07-16 23:56:56 +0900

    fix a bug

--- budgetbook/init_db.py
+++ budgetbook/init_db.py
@@ -2,7 +2,11 @@ import sqlite3
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "db", "budgetbook.db")
+
+if os.getenv("TESTING") == "1":
+    DB_PATH = os.path.join(BASE_DIR, "db", "budgetbook_test.db")
+else:
+    DB_PATH = os.path.join(BASE_DIR, "db", "budgetbook.db")
 
 schema = """
 CREATE TABLE IF NOT EXISTS income (
