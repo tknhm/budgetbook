464fb4dcfac87ced16b6c8552c524866c1e9becb
tsu__ko <natsu.nhm04@gmail.com>
2025-07-29 23:46:13 +0900

    支払い方法のバリデーションを変更

--- budgetbook/schemas.py
+++ budgetbook/schemas.py
@@ -19,9 +19,7 @@ class ExpenseSchema(ma.Schema):
     shop = fields.Str(required=True, validate=validate.Length(min=1))
     category = fields.Str(required=True)
     amount = fields.Int(required=True, validate=validate.Range(min=1))
-    payment = fields.Str(
-        required=True, validate=validate.OneOf(["現金", "カード", "PayPay", "ルミネカード"])
-    )
+    payment = fields.Str(required=True)
 
 
 expense_schema = ExpenseSchema()
